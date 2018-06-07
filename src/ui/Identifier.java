65c091efff7d53c2f6ff54f21717ae156b5cdb2a
Mizushinku <frankie786214593@gmail.com>
2018-06-07 23:26:10 +0800

    QQ

--- src/ui/Identifier.java
+++ src/ui/Identifier.java
@@ -16,7 +16,7 @@ public class Identifier {
 	final static String AddFriend = "\tAddfriend";
 	final static String AddFriendS = "\tAddfriendS";
 	final static String AddFriendF = "\tAddfriendF";
-	final static String AddGroup = "\tAddGroup";
+	final static String AddGroup = "\tAddGroup";	
 	final static String AddGroupS = "\tAddGroupS";
 	final static String AddGroupF = "\tAddGroupF";
 	final static String FetchRecord = "\tFetchRecord";
